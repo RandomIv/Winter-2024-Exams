a179c9c768af1998a3cf3c8702e21bfd88f24b06
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-13 20:37:11 +0200

    Remove unnecessary variable

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,17 +1,15 @@
 // Extract substring between prefix and suffix
 
-// Step 1
-// Added 'use strict';
-// Added const for function and constants
+// Step 2
+// Remove unnecessary variable
 
 'use strict'
 
 const getvaluebetween = (str, p, s) => {
-  const i = str.indexOf(p);
+  const i = str.indexOf(p) + p.length;
   if (i === -1) return '';
   else {
-    const k = i + p.length;
-    str = str.substring(k);
+    str = str.substring(i);
     if (s) {
       const i = str.indexOf(s);
       if (i === -1) {
