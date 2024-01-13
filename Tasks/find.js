5ebdc59232086d5c48296a8905da1f5db646f077
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-14 00:34:12 +0200

    Added use strict and added const for function and constants

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,9 +1,11 @@
 // Find key by value
 
-find = (object, ...rest) => {
-  value = rest.pop(1);
+'use strict';
+
+const find = (object, ...rest) => {
+  const value = rest.pop(1);
   [];
-  for (name in object) {
+  for (const name in object) {
     if (object[name] !== value) {
     } else {
       if (typeof name) return name;
