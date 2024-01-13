5012727acd6dbf3d73e5b50faf81cc4616c15f0e
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-13 20:46:03 +0200

    Fused if statement into one and changed order of lines

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,18 +1,16 @@
 // Extract substring between prefix and suffix
 
-// Step 4
-// Removed unnecessary if...else statements
+// Step 5
+// Fused if statement into one
+// Changed order of lines
 
 'use strict'
 
 const getValueBetween = (string, prefix, sufix) => {
   const begin = string.indexOf(prefix) + prefix.length;
-  if (begin === -1) return '';
   string = string.substring(begin);
   const end = string.indexOf(sufix);
-  if (end === -1) {
-    return '';
-  }
+  if (begin === -1 || end === -1) return '';
   string = string.substring(0, end);
   return string;
 };
