939efd9440ac0eee2232b25d7ea7e7003c763e84
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-13 20:48:28 +0200

    Fused 11, 14, 15 lines into one

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,18 +1,15 @@
 // Extract substring between prefix and suffix
 
-// Step 5
-// Fused if statement into one
-// Changed order of lines
+// Step 6
+// Fused 11, 14, 15 lines into one
 
 'use strict'
 
 const getValueBetween = (string, prefix, sufix) => {
   const begin = string.indexOf(prefix) + prefix.length;
-  string = string.substring(begin);
   const end = string.indexOf(sufix);
   if (begin === -1 || end === -1) return '';
-  string = string.substring(0, end);
-  return string;
+  return string.substring(begin, end);
 };
 
 module.exports = getValueBetween;
