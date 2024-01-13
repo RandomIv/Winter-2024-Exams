28afd9401a48624b33c538d1266c4444ba63b67d
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-13 21:24:04 +0200

    Removed unnecessary if...else statement

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -3,14 +3,11 @@
 'use strict'
 
 const duplicate = (value, duplicatesAmount) => {
-  if (duplicatesAmount <= 0) return [];
-  else {
-    const text = [];
-    for (let i = 0; i < duplicatesAmount; i++) {
-      text[i] = value;
-    }
-    return text;
+  const text = [];
+  for (let i = 0; i < duplicatesAmount; i++) {
+    text[i] = value;
   }
+  return text;
 };
 
 module.exports = duplicate;
