b3505a21be3476d7592feb369a32ea43c3ae3a14
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-13 21:03:40 +0200

    Added use strict directive and added const for function and constants

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,9 +1,11 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
+'use strict'
+
+const duplicate = (value, N) => {
   if (N <= 0) return [];
   else {
-    res = [];
+    const res = [];
     for (let i = 0; i < N; i++) {
       res[i] = value;
     }
