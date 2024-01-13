3b0bdb4415649eee619cea28f25a64e24ea2c65f
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-13 20:38:58 +0200

    Removed unnecessary if...else statements

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,24 +1,19 @@
 // Extract substring between prefix and suffix
 
-// Step 3
-// Formatted naming
+// Step 4
+// Removed unnecessary if...else statements
 
 'use strict'
 
 const getValueBetween = (string, prefix, sufix) => {
   const begin = string.indexOf(prefix) + prefix.length;
   if (begin === -1) return '';
-  else {
-    string = string.substring(begin);
-    if (sufix) {
-      const end = string.indexOf(sufix);
-      if (end === -1) {
-        return '';
-      } else {
-        string = string.substring(0, end);
-      }
-    }
+  string = string.substring(begin);
+  const end = string.indexOf(sufix);
+  if (end === -1) {
+    return '';
   }
+  string = string.substring(0, end);
   return string;
 };
 
