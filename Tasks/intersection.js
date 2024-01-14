176712f13b1d42f039a4250fac76cffea0603976
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-14 03:23:23 +0200

    Simplified if...else structure and created result object that will be returned

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,14 +3,13 @@
 'use strict';
 
 const intersectDictionaries = function intersectDictionaries(object1, object2) {
+  const result = {};
   for (const key of Object.keys(object1)) {
-    if (object1[key] === object2[key]) {
-      object2[key] = object1[key];
-    } else {
-      delete object1[key];
+    if (object2.hasOwnProperty(key) && object1[key] === object2[key]) {
+      result[key] = object1[key];
     }
   }
-  return object1;
+  return result;
 };
 
 module.exports = intersectDictionaries;
