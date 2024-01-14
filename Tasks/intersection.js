b132d450030376af49c69dca54bfbf0e4bcadcbf
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-14 03:37:38 +0200

    Changed for...of loop and changed function into arrow function

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,14 +2,14 @@
 
 'use strict';
 
-const intersectDictionaries = function intersectDictionaries(object1, object2) {
+const intersectDictionaries = (dict1, dict2) => {
   const result = {};
-  for (const key of Object.keys(object1)) {
-    if (object2.hasOwnProperty(key) && object1[key] === object2[key]) {
-      result[key] = object1[key];
+  for (const [key, value] of Object.entries(dict1)) {
+    if (dict2.hasOwnProperty(key) && value === dict2[key]) {
+      result[key] = value;
     }
   }
   return result;
-};
+}
 
 module.exports = intersectDictionaries;
