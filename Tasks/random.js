916b6ef1a7f1e6090d46ce288a6c035071006fa6
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-14 05:30:52 +0200

    Removed unnecessary if...else statement

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,13 +2,8 @@
 
 'use strict';
 
-const randomIntegerInRange = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
+const randomIntegerInRange = (min, max = min) => {
+  return min + Math.floor(Math.random() * (max - min + 1));
 };
 
 module.exports = randomIntegerInRange;
