6d07df1cfcfb130f41b1d29bbcd61ef0d6226474
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-14 06:33:46 +0200

    only is fixed

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,6 +1,9 @@
 // Copy only listed values from dict
 
+//only is fixed. Test is passed: 7 of 7
+
 'use strict';
+
 const onlyListedValues = (object, ...values) => {
   for(const key of Object.keys(object)){
     if (!values.includes(key)) {
