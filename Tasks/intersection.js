c2b93d4cddc4033f99363013cacf93877bc80596
RandomIv <ivanishin.alexander@lll.kpi.ua>
2024-01-14 03:17:01 +0200

    Removed unnecessary identifiers

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,8 +3,7 @@
 'use strict';
 
 const intersectDictionaries = function intersectDictionaries(object1, object2) {
-  const firstKeys = Object.keys(object1);
-  for (const key of firstKeys) {
+  for (const key of Object.keys(object1)) {
     if (object1[key] === object2[key]) {
       object2[key] = object1[key];
     } else {
